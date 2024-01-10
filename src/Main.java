e595d2abc438ef772b56274e9664faa8688835e1
mumud1 <155530032+mumud1@users.noreply.github.com>
2024-01-10 10:59:21 +0800

    added comment

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        //hello
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
