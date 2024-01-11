22b40c609e9c4e2f7d447d7699cb31b6a00e197a
mumud1 <155530032+mumud1@users.noreply.github.com>
2024-01-11 10:43:01 +0800

    added the following methods: calculationProteinHelper/calculationCaloriesHelper (To help out the calculationProtein and calculationCalories method) some important mutator and getter methods for private variables add method that adds in the ingredient along with its portion and storing nutrient levels as well as updating the protein and calories of the meal.

--- src/Recipe.java
+++ src/Recipe.java
@@ -3,6 +3,7 @@ public class Recipe {
     private ArrayList<Ingredient> ingredients = new ArrayList<>();
     private ArrayList<Double> portions = new ArrayList<>();
     private double[] nutrients = new double[13];
+    private final Vitamins[] nutrientBase = {A, B1, B2, B3, B5, B6, B7, B9, B12, C, D, E, K};
     private String name;
     private String description;
     private MealType mealType;
@@ -25,6 +26,35 @@ public class Recipe {
             iter++;
         }
     }
+
+    public double calculationProteinHelper(Ingredient ingredient, double portion)
+    {
+        int result = (portion / 100) * ingredient.getProteinPer100();
+        return result;
+    }
+
+    public void calculationProtein()
+    {
+        for(int i = 0 ; i < ingredients.size() ; i++)
+        {
+            this.protein += calculationCaloriesHelper(ingredients.get(i), portions.get(i));
+        }
+    }
+
+    public double calculationCaloriesHelper(Ingredient ingredient, double portion)
+    {
+        int result = (portion / 100) * ingredient.getCaloriesPer100();
+        return result
+    }
+
+    public void calculationCalories()
+    {
+        for(int i = 0 ; i < ingredients.size() ; i++)
+        {
+            this.calories += calculationCaloriesHelper(ingredients.get(i), portions.get(i));
+        }
+    }
+
     public String getName() {
         return name;
     }
@@ -34,13 +64,32 @@ public class Recipe {
     }
 
     public String getDescription() {
-        return description;
+        return this.description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public void setMealType(MealType mealType){ this.mealType = mealType; }
+
+    public void getMealType() { return this.mealType; }
+
+    public double getProtein() { return this.protein; }
+
+    public double getCalories() { return this.calories; }
+
+    public void add(Ingredient ingredient, double portion)
+    {
+        this.ingredients.add(ingredient);
+        this.portion.add(portion);
+        for(int i = 0 ; i < nutrientBase.length ; i++)
+        {
+            nutrients[i] += ingredient.getNutrientAmount(nutrientBase[i]);
+        }
+        calculateProtein();
+        calculateCalories();
+    }
 
 
 }
